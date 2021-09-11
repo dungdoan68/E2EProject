601ffc83e9f5bfc9bfd14cd99967d5d95621bae6
Dung <adung.doan@gmail.com>
2021-09-11 11:22:42 +0700

    chang form GIT STUFF

--- src/test/java/Tutorial/HomePage.java
+++ src/test/java/Tutorial/HomePage.java
@@ -27,6 +27,7 @@ public class HomePage extends Base {
 	}
 	
 	@Test(dataProvider = "getData")
+	
 	public void basePageNavigation(String userName, String passWord, String text) throws IOException {
 		
 		webDriver.get(prop.getProperty("baseURL"));
@@ -49,7 +50,7 @@ public class HomePage extends Base {
 				{"mngr1336","dAnavUq"},
 				{"invalid","valid"},
 				{"valid","invalid"},
-				{"invalid","invalid"}
+
 			};
 		}
 		else {
